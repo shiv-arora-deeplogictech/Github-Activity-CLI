fd3286b1617266ed4e15db347ed75b1228814e78
Shiv Arora <shiv.arora@deeplogictech.com>
2025-02-04 15:57:02 +0530

    Added the user activity

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 #!/usr/bin/env node
+
+import axios from "axios";
+import chalk from "chalk";
+import {Command} from "commander";
+import ora from "ora";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const BASE_URL = "https://api.github.com";
+
+const fetchActivity = async (username) => {
+    const spinner = ora(`Fetching the data for ${username}`).start();
+
+    try{
+        const response = await axios.get(`${BASE_URL}/users/${username}/events`,{
+            header:{
+                Authorization :`token ${process.env.GITHUB_TOKEN}`,
+                Accept : "application/vnd.github+json",
+            },
+        });
+
+        spinner.stop(); 
+
+        if(response.length===0){
+            console.log(chalk.yellow("No recent activity Found !"));
+            return;
+        }
+        console.log(chalk.green.bold(`Recent activity for ${username}`));
+        response.data.slice(0,5).forEach((event,index) => {
+            console.log(
+                chalk.blue(`${index+1}. ${event.type} -> ${event.repo.name}`)
+            );
+        });
+    } catch (error){
+        spinner.stop();
+        console.error(chalk.red("Error Fetching Activity !!"),error.message);
+    }
+}
+
+const program = new Command();
+
+program
+    .version("1.0.0")
+    .description("Github Activity Tracker CLI")
+    .argument("<username>","Github Username")
+    .action((username) => {
+        fetchActivity(username);
+    });
+
+program.parse(process.argv);
+
